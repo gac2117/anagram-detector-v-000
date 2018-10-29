6be9b3d9836c248ea4033f0f8a948da0da7f7c05
Grace Choi <oiograceoio@gmail.com>
2018-10-29 11:03:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :anagrams
+
+  def initialize(word)
+    @anagrams = word
+  end
+
+  def match(array)
+    @anagrams == array.split("")
+  end
+  
